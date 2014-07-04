60b631cd310a514b05a378343ce39b3eef87f176
Bart Bakker <bart@thesoftwarecraft.com>
2014-07-04 11:10:45 +0200

    extract display function

--- app.c
+++ app.c
@@ -5,6 +5,8 @@
 #include "tasks.h"
 #include "todotxt.h"
 
+char *_format_task_for_display(Task* t);
+
 int main(void) {
 	TodoTxt *todo = todotxt_open("todo.txt");
 	if (!todo) { puts("Cannot open todo.txt"); return 1; }
@@ -14,19 +16,25 @@ int main(void) {
 	tasklist_sort_by_priority(list, prioritized);
 
 	for (int i = 0; i < prioritized->len; ++i) {
-		Task *t = prioritized->tasks[i];
+		char *formatted = _format_task_for_display(prioritized->tasks[i]);
+		puts(formatted);
+		free(formatted);
 
-		char *display = malloc(strlen(t->description) + 3);
-		if ('\0' != t->priority) {
-			display[0] = t->priority; display[1] = ':'; display[2] = ' ';
-		} else {
-			strcat(display, "   ");
-		}
-		strcpy(&display[3], t->description);
-
-		puts(display);
 	}
 	free_tasklist(prioritized);
 	free_tasklist(list);
 	todotxt_close(todo);
 }
+
+char *_format_task_for_display(Task* t) {
+	char *display = malloc(strlen(t->description) + 3);
+	if ('\0' != t->priority) {
+		display[0] = t->priority; display[1] = ':'; display[2] = ' ';
+	} else {
+		strcat(display, "   ");
+	}
+	strcpy(&display[3], t->description);
+
+	return display;
+}
+
